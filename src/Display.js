9f2e1513ea0d9436550a02c49129d280d23ccc2a
het <gandhihet12@gmail.com>
2021-05-05 22:06:06 +0530

    5-5-21

---
+++ src/Display.js
@@ -0,0 +1,262 @@
+import React, { useEffect, useState } from "react";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardHeader from "@material-ui/core/CardHeader";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { Link } from "react-router-dom";
+import AddIcon from "@material-ui/icons/Add";
+import MenuItem from "@material-ui/core/MenuItem";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import "./Display.css";
+
+const useStyles = makeStyles((theme) => ({
+  "@global": {
+    ul: {
+      margin: 0,
+      padding: 0,
+      listStyle: "none",
+    },
+  },
+  appBar: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+  toolbar: {
+    flexWrap: "wrap",
+  },
+  toolbarTitle: {
+    flexGrow: 1,
+  },
+  link: {
+    margin: theme.spacing(1, 1.5),
+  },
+  heroContent: {
+    padding: theme.spacing(8, 0, 6),
+  },
+  cardHeader: {
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[200]
+        : theme.palette.grey[700],
+  },
+  cardPricing: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "baseline",
+    marginBottom: theme.spacing(2),
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 200,
+    marginLeft: 50,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+export default function Pricing() {
+  const classes = useStyles();
+  const [data, setData] = useState({});
+  const [mainData, setMainData] = useState({});
+  const [brand, setBrand] = useState([]);
+  const [filterBrand, setFilterBrand] = useState();
+  const [dataBrand, setDataBrand] = useState([]);
+  const [dataFilter, setDataFilter] = useState([]);
+
+  const unique = (value, index, self) => {
+    return self.indexOf(value) === index;
+  };
+
+  useEffect(() => {
+    var d = JSON.parse(localStorage.getItem("data"));
+    setData(d);
+    setMainData(d);
+    setDataBrand(d);
+    setDataFilter(d);
+    var p = [];
+    if (d?.length > 0) {
+      d.map((item) => p.push(item.brand));
+      const unique1 = p.filter(unique);
+      unique1.push("All");
+      unique1.reverse();
+      setBrand(unique1);
+    }
+  }, []);
+
+  const handleBrand = (e) => {
+    var c = e.target.value;
+    console.log(c);
+    setFilterBrand(c);
+    var g = [];
+    if (c === "All") {
+      g = mainData;
+      setDataBrand(mainData);
+    } else {
+      var g = mainData.filter((item) => item.brand === c);
+      setDataBrand(g);
+    }
+    const filteredArray = g.filter((value) => dataFilter.includes(value));
+    console.log(filteredArray);
+    setData(filteredArray);
+  };
+
+  const handleFilter = (e) => {
+    console.log(e.target.value);
+    var lower = e.target.value.split("-")[0];
+    var higher = e.target.value.split("-")[1];
+    console.log(lower, higher);
+    var g = [];
+    if (lower === "20000 and more") {
+      g = mainData.filter((item) => Number(item.price) >= 20000);
+      setDataFilter(g);
+      console.log(g);
+    } else if (lower === "All") {
+      g = mainData;
+      setDataFilter(mainData);
+    } else {
+      g = mainData.filter(
+        (item) =>
+          Number(item.price) >= Number(lower) &&
+          Number(item.price) <= Number(higher)
+      );
+      setFilterBrand(g);
+    }
+    const filteredArray = g.filter((value) => dataBrand.includes(value));
+    setData(filteredArray);
+  };
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+
+      <Container maxWidth="sm" component="main" className={classes.heroContent}>
+        <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          color="textPrimary"
+          gutterBottom
+        >
+          Mobiles
+        </Typography>
+      </Container>
+      <div className="navbar">
+        <Link
+          to="/form"
+          className="nav-item"
+          style={{ textDecoration: "none", marginTop: 14 }}
+        >
+          <Button variant="contained" color="primary">
+            <AddIcon /> &nbsp;Add Details
+          </Button>
+        </Link>
+
+        <FormControl className={classes.formControl}>
+          <InputLabel id="demo-simple-select-label">Brand</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={filterBrand}
+            onChange={handleBrand}
+          >
+            {brand?.map((item) => (
+              <MenuItem
+                name={item}
+                value={item}
+                onClick={(e) => handleBrand(e)}
+              >
+                {item}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        <FormControl className={classes.formControl}>
+          <InputLabel id="demo-simple-select-label1">Price</InputLabel>
+          <Select
+            labelId="demo-simple-select-label1"
+            id="demo-simple-select1"
+            value={filterBrand}
+            onChange={handleFilter}
+          >
+            <MenuItem value="All">All</MenuItem>
+            <MenuItem value="0-5000">0-5000</MenuItem>
+            <MenuItem value="5000-10000">5000-10000</MenuItem>
+            <MenuItem value="10000-15000">10000-15000</MenuItem>
+            <MenuItem value="15000-20000">15000-20000</MenuItem>
+            <MenuItem value="20000 and more">20000 and more</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+
+      <Container maxWidth="md" component="main">
+        <Grid container spacing={5} alignItems="flex-end">
+          {data?.length > 0 ? (
+            data.map((tier) => (
+              <Grid item key={tier.mobile_name} xs={12} md={4}>
+                <Card>
+                  <CardHeader
+                    title={tier.mobile_name}
+                    subheader={tier.brand}
+                    titleTypographyProps={{ align: "center" }}
+                    subheaderTypographyProps={{ align: "center" }}
+                    className={classes.cardHeader}
+                  />
+                  <CardContent>
+                    <div className={classes.cardPricing}>
+                      <Typography
+                        component="h2"
+                        variant="h3"
+                        color="textPrimary"
+                      >
+                        {tier.price}₹
+                      </Typography>
+                    </div>
+                    <ul>
+                      <Typography
+                        component="li"
+                        variant="subtitle1"
+                        align="center"
+                      >
+                        color:{tier.color}
+                      </Typography>
+                    </ul>
+                    <ul>
+                      <Typography
+                        component="li"
+                        variant="subtitle1"
+                        align="center"
+                      >
+                        RAM: {tier.ram}GB
+                      </Typography>
+                    </ul>
+                    <ul>
+                      <Typography
+                        component="li"
+                        variant="subtitle1"
+                        align="center"
+                      >
+                        ROM: {tier.rom}GB
+                      </Typography>
+                    </ul>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))
+          ) : data ? (
+            <h1>No result found for this filter</h1>
+          ) : (
+            <h1>Add Mobiles to Show</h1>
+          )}
+        </Grid>
+      </Container>
+    </React.Fragment>
+  );
+}
---
+++ src/Form.js
@@ -0,0 +1,202 @@
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import "./Form.css";
+import { useHistory } from "react-router";
+import { Link } from "react-router-dom";
+import AddIcon from "@material-ui/icons/Add";
+import HomeIcon from "@material-ui/icons/Home";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignUp() {
+  const classes = useStyles();
+  const [details, setDetails] = useState({
+    mobile_name: "",
+    brand: "",
+    ram: "",
+    rom: "",
+    color: "",
+    price: "",
+  });
+  const history = useHistory();
+
+  const handleChange = (e) => {
+    setDetails({ ...details, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    if (
+      details.mobile_name === "" ||
+      details.brand === "" ||
+      details.ram === "" ||
+      details.rom === "" ||
+      details.color === "" ||
+      details.price === ""
+    ) {
+      alert("Please do not leave any input empty");
+    } else {
+      e.preventDefault();
+      var d = JSON.parse(localStorage.getItem("data"));
+      console.log(d);
+      if (d === null) {
+        var a = [];
+        a.push(details);
+        localStorage.setItem("data", JSON.stringify(a));
+      } else {
+        d.push(details);
+        localStorage.setItem("data", JSON.stringify(d));
+      }
+      history.push("/");
+    }
+  };
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          Add Mobile Details
+        </Typography>
+        <form
+          className={classes.form}
+          noValidate
+          onSubmit={(e) => handleSubmit(e)}
+        >
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                label="Mobile Name"
+                name="mobile_name"
+                value={details.mobile_name}
+                autofocus
+                onChange={(e) => handleChange(e)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                label="Brand"
+                name="brand"
+                value={details.brand}
+                onChange={(e) => handleChange(e)}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="ram"
+                variant="outlined"
+                required
+                fullWidth
+                label="RAM"
+                type="number"
+                value={details.ram}
+                onChange={(e) => handleChange(e)}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">GB</InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                label="ROM"
+                name="rom"
+                type="number"
+                value={details.rom}
+                onChange={(e) => handleChange(e)}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">GB</InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="color"
+                variant="outlined"
+                required
+                fullWidth
+                value={details.color}
+                onChange={(e) => handleChange(e)}
+                label="Color"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                label="price"
+                name="price"
+                type="number"
+                value={details.price}
+                onChange={(e) => handleChange(e)}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">GB</InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            <AddIcon /> &nbsp;Add Details
+          </Button>
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              <HomeIcon />
+              &nbsp; Go to Home
+            </Button>
+          </Link>
+        </form>
+      </div>
+    </Container>
+  );
+}
